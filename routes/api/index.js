bb02542bdf35069c62983da3d75465d92186ec91
Shivam Nema <nemashivam5@gmail.com>
2018-04-07 16:47:32 +0530

    Create profiles endpoint

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/', require('./users'))
+router.use('/profiles', require('./profiles'))
 
 router.use(function(err, req, res, next) {
     if (err.name === 'ValidationError') {
